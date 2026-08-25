25dcd86bdf12b1b6e1281ec1f7fdcc91f3bc92d9
Motheo-365 <u24666981@tuks.co.za>
2026-08-25 17:39:14 +0200

    init2

--- express-server/src/server.ts
+++ express-server/src/server.ts
@@ -1,9 +1,16 @@
-// Starts the application
 import "./config.js";
 import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Roamly API running on port ${PORT}`);
+});
+
+server.on("error", (error) => {
+    console.error("SERVER ERROR:", error);
+});
+
+server.on("close", () => {
+    console.log("SERVER CLOSED");
 });
